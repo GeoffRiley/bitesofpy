6331fc6e96ce5135e7ee087fa65829936a6b3a13
Geoff Riley <geoffr@geoff-ubuntu.adaso.com>
2019-10-26 23:07:45 +0100

    submission Bite 224 @ codechalleng.es (improved)

--- 224/split.py
+++ 224/split.py
@@ -4,4 +4,4 @@ import re
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    return re.findall(r'\b[A-Z].+?[.?!](?= +[A-Z])', text.replace('\n', ' ') + ' X', re.DOTALL)
+    return re.findall(r'\b[A-Z].+?[.?!](?= +[A-Z]|$)', text.replace('\n', ' ').strip(), re.DOTALL)
