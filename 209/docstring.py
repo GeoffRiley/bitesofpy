022bc91e925240e7b6656a341bb3115ff1962ad2
Geoff Riley <geoffr@geoff-ubuntu.adaso.com>
2019-11-04 23:30:31 +0000

    submission Bite 209 @ codechalleng.es revised

--- 209/docstring.py
+++ 209/docstring.py
@@ -2,8 +2,10 @@ def sum_numbers(numbers):
     """Sums numbers
 
     :param numbers: a list of numbers
-    :type numbers: int
+    :type numbers: list
+
     :raises TypeError: if not all numeric values passed in
+
     :return: sum of numbers
     :rtype: int
     """
