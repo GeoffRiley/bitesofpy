0a37351b1cd6dca59197e681f055cd5be14a51a3
Geoff Riley <geoffr@geoff-ubuntu.adaso.com>
2019-11-19 13:08:30 +0000

    submission Bite 240 @ codechalleng.es updated

--- 240/test_account.py
+++ 240/test_account.py
@@ -23,6 +23,8 @@ def test_account_create(einstein):
     assert einstein.balance == 100
     assert str(einstein) == 'Account of Einstein with starting amount: 100'
     assert repr(einstein) == "Account('Einstein', 100)"
+    hector = Account('Hector')
+    assert repr(hector) == "Account('Hector', 0)"
 
 
 def test_account_transaction(einstein):
@@ -36,8 +38,9 @@ def test_account_transaction(einstein):
 
 def test_account_bad_transaction(socrates):
     assert socrates.balance == 0
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as exp:
         socrates.add_transaction(3.14)
+    assert 'please use int for amount' in str(exp.value)
     assert socrates.balance == 0
 
 
@@ -46,9 +49,12 @@ def test_account_comparisons(einstein, socrates):
     add_transactions(socrates, [10, 20, 30])
     assert socrates.balance == 60
     assert socrates[1] == 20
+    socrates.add_transaction(40)
+    assert einstein == socrates
+    assert not (einstein < socrates)
 
 
-def test_account_merge_acounts(einstein, socrates):
+def test_account_merge_accounts(einstein, socrates):
     assert einstein.balance == 100 and socrates.balance == 0
     add_transactions(einstein, [50, -75])
     add_transactions(socrates, [10, 20, 30])
@@ -58,3 +64,13 @@ def test_account_merge_acounts(einstein, socrates):
     assert str(pythagoras) == 'Account of Einstein&Socrates with starting amount: 100'
     assert len(pythagoras) == 5
     assert pythagoras[1] == -75
+
+
+def test_account_bad_merge_accounts(einstein, socrates):
+    add_transactions(einstein, [50, -75])
+    add_transactions(socrates, [10, 20, 30])
+    kelvin = Account('Kelvin', 20)
+    assert kelvin.balance == 20
+    armstrong = kelvin + einstein + socrates
+    assert armstrong.balance == 155
+    assert str(armstrong) == 'Account of Kelvin&Einstein&Socrates with starting amount: 120'
