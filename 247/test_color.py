d686bfcda666e77468481a2cb733dcb48c4f6c4f
Geoff Riley <gir6@ou.ac.uk>
2019-12-30 07:23:50 +0000

    submission Bite 247 @ codechalleng.es updated to pass mutpy

--- 247/test_color.py
+++ 247/test_color.py
@@ -12,12 +12,18 @@ def gen():
 
 @pytest.mark.parametrize('test_values, result', [
     ([1, 2, 3], '#010203'),
-    ([0, 0, 0], '#000000'),
-    ([255,255,255], '#FFFFFF'),
-    ([-1,-1,-1], '#-1-1-1'),
-    ([256,256,256], '#100100100')
+    ([0, 1, 2], '#000102'),
+    ([255, 254, 253], '#FFFEFD'),
+    ([-1, -2, -3], '#-1-2-3'),
+    ([254, 255, 256], '#FEFF100'),
+    ([1, 2, 3, 4], pytest.raises(ValueError))
 ])
 @patch('color.sample')
 def test_gen_hex_color(mock_sample, gen, test_values, result):
     mock_sample.return_value = test_values
-    assert next(gen) == result
+    if isinstance(result, str):
+        assert next(gen) == result
+    else:
+        with result:
+            assert next(gen)
+    mock_sample.assert_called_with(range(0, 256), 3)
