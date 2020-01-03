bfa5b35bf0cf59975c389b2712ae994b474d006b
Geoff Riley <geoffr@adaso.com>
2020-01-03 17:55:41 +0000

    submission Bite 251 @ codechalleng.es

---
+++ 251/series.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def basic_series() -> pd.Series:
+    """Create a pandas Series containing the values 1, 2, 3, 4, 5
+    Don't worry about the indexes for now.
+    The name of the series should be 'Fred'
+    """
+    return pd.Series([n + 1 for n in range(5)], name='Fred')
+
+
+def float_series() -> pd.Series:
+    """Create a pandas Series containing the all the values
+    from 0.000 -> 1.000 e.g. 0.000, 0.001, 0.002... 0.999, 1.000
+    Don't worry about the indexes or the series name.
+    """
+    return pd.Series([n / 1000 for n in range(1001)])
+
+
+def alpha_index_series() -> pd.Series:
+    """Create a Series with values 1, 2, ... 25, 26 of type int64
+    and add an index with values a, b, ... y, z
+    so index 'a'=1, 'b'=2 ... 'y'=25, 'z'=26
+    Don't worry about the series name.
+    """
+    return pd.Series([n + 1 for n in range(26)], index=[chr(97 + n) for n in range(26)])
+
+
+def object_values_series() -> pd.Series:
+    """Create a Series with values A, B, ... Y, Z of type object
+    and add an index with values 101, 102, ... 125, 126
+    so index 101='A', 102='B' ... 125='Y', 126='Z'
+    Don't worry about the series name.
+    """
+    return pd.Series([chr(65 + n) for n in range(26)], index=[n + 101 for n in range(26)])
