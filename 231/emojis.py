8eae8c6db97ad09d5efdceb1445567e191604f06
Geoff Riley <geoffr@adaso.com>
2020-01-10 23:30:06 +0000

    submission Bite 231 @ codechalleng.es

---
+++ 231/emojis.py
@@ -0,0 +1,11 @@
+import re
+from typing import List
+
+# https://stackoverflow.com/a/43147265
+# just for exercise sake, real life use emoji lib
+IS_EMOJI = re.compile(r'[^\w\s,]')
+
+
+def get_emoji_indices(text: str) -> List[int]:
+    """Given a text return indices of emoji characters"""
+    return [i for i, c in enumerate(text) if not c.isascii()]
