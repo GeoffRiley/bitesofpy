5a6831c04d4075f6cad599fd85b48f7775ba471f
Geoff Riley <geoffr@adaso.com>
2022-11-26 16:22:38 +0000

    submission Bite 360 @ codechalleng.es

---
+++ 360/test_script.py
@@ -0,0 +1,18 @@
+import re
+
+import pytest
+from typer.testing import CliRunner
+
+from script import app
+
+
+@pytest.fixture()
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+def test_progress(capfd, runner):
+    result = runner.invoke(app, [])
+    pat = re.compile(r'^Processing... ━+ 100% 0:\d+:\d+\nProcessed \d+ things.$')
+    assert result.exit_code == 0
+    assert pat.match(result.output.strip())
